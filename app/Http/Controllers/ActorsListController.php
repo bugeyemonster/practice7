8674f7e459d04207f7b7dfd36e29ae7f029fd20d
j_polak <jan.polak@onmyway.cz>
2016-11-21 12:21:36 +0100

    The actors list controller & route

---
+++ app/Http/Controllers/ActorsListController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ActorsListController extends Controller
+{
+    //
+    public function index()
+    {
+        return 'THIS IS THE LIST OF ACTORS';
+    }
+}
